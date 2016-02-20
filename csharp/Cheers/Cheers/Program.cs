62e092f728b9a0276dd470d04e66e47dc35ac994
Walker Dorris <walker.dorris@gmail.com>
2016-02-20 11:15:04 -0800

    part 1 and 2 working, but might consider rounding result for days until bday

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -43,6 +43,25 @@ namespace Cheers
             }
             Console.WriteLine(name + " is awesome!");
 
+            DateTime dt = Convert.ToDateTime(birthday);
+
+            //Console.WriteLine("Month: {0}, Day: {1}", dt.Month, dt.Day);
+
+            DateTime localDate = DateTime.Now;
+
+            //Console.WriteLine("Month Today: {0}, Day Today: {1}", localDate.Month, localDate.Day);
+            TimeSpan t = localDate - dt;
+            //Console.WriteLine("Days until your birthday " + t.TotalDays);
+            if (t.TotalDays >= 0 && t.TotalDays < 1)
+            {
+                Console.WriteLine("Happy Birthday!");
+            }
+            else
+            {
+                Console.WriteLine("Days until your birthday: " + Math.Abs(t.TotalDays));
+            }
+
+
             Console.Read();
         }
     }
